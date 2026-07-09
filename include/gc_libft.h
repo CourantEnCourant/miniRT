89b6f77fa1b188087621f3c1ffde581fd0276f75
Weiqi Zhang <weiqi.zhang_arthur@yahoo.com>
2026-07-09 12:17:23 +0200

    feat: gc_split returns t_darray * and not void **

--- include/gc_libft.h
+++ include/gc_libft.h
@@ -14,14 +14,15 @@
 # define GC_LIBFT_H
 
 # include <stddef.h>
+# include "datastructures.h"
 # include "gc.h"
 
-char	*gc_get_next_line(int fd, t_gc *gc);
-char	*gc_itoa(int n, t_gc *gc);
-char	**gc_split(char const *s, char c, t_gc *gc);
-char	*gc_strdup(char *s, t_gc *gc);
-char	*gc_strjoin(char const *s1, char const *s2, t_gc *gc);
-char	*gc_strtrim(char const *s1, char const *set, t_gc *gc);
-char	*gc_substr(char const *s, unsigned int start, size_t len, t_gc *gc);
+char		*gc_get_next_line(int fd, t_gc *gc);
+char		*gc_itoa(int n, t_gc *gc);
+t_darray	*gc_split(char const *s, char c, t_gc *gc);
+char		*gc_strdup(char *s, t_gc *gc);
+char		*gc_strjoin(char const *s1, char const *s2, t_gc *gc);
+char		*gc_strtrim(char const *s1, char const *set, t_gc *gc);
+char		*gc_substr(char const *s, unsigned int start, size_t len, t_gc *gc);
 
 #endif
