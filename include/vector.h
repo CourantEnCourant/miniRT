97a88e9daaedc1f160b65fd151e2bfc206869b5d
Weiqi Zhang <weiqi.zhang_arthur@yahoo.com>
2026-07-15 04:05:42 +0200

    feat: add more vector methods

--- include/vector.h
+++ include/vector.h
@@ -28,6 +28,10 @@ typedef struct s_vec3
 t_vec3	cross_product(t_vec3 v1, t_vec3 v2);
 double	dot_product(t_vec3 v1, t_vec3 v2);
 t_vec3	vec3_add(t_vec3 v1, t_vec3 v2);
+double	vec3_mod(t_vec3 v);
+t_vec3	vec3_normalize(t_vec3 v);
 t_vec3	vec3_scal_mult(t_vec3 v1, double scale);
+t_vec3	vec3_sub(t_vec3 v1, t_vec3 v2);
+t_vec3	vec3_scal_div(t_vec3 v1, double scale);
 
 #endif
--- lib/vector/vector.c
+++ lib/vector/vector.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "math.h"
 #include "vector.h"
 
 t_vec3	cross_product(t_vec3 v1, t_vec3 v2)
@@ -33,9 +34,31 @@ t_vec3	vec3_add(t_vec3 v1, t_vec3 v2)
 			v1.arr[Z] + v2.arr[Z]}});
 }
 
+double	vec3_mod(t_vec3 v)
+{
+	return (sqrt(dot_product(v, v)));
+}
+
+t_vec3	vec3_normalize(t_vec3 v)
+{
+	return (vec3_scal_div(v, vec3_mod(v)));
+}
+
 t_vec3	vec3_scal_mult(t_vec3 v1, double scale)
 {
 	return ((t_vec3){{v1.arr[X] * scale,
 			v1.arr[Y] * scale,
 			v1.arr[Z] * scale}});
+}
+
+t_vec3	vec3_sub(t_vec3 v1, t_vec3 v2)
+{
+	return ((t_vec3){{v1.arr[X] - v2.arr[X],
+			v1.arr[Y] - v2.arr[Y],
+			v1.arr[Z] - v2.arr[Z]}});
+}
+
+t_vec3	vec3_scal_div(t_vec3 v, double scale)
+{
+	return (vec3_scal_mult(v, 1.0 / scale));
 }
