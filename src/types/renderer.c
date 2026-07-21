348e038ad278fb434605cbb823e129e2bbc5573b
Weiqi Zhang <weiqi.zhang_arthur@yahoo.com>
2026-07-21 07:24:06 +0200

    feat: render a gradient image

--- src/types/renderer.c
+++ src/types/renderer.c
@@ -16,24 +16,38 @@
 #include "geometry.h"
 #include "mlx.h"
 #include "minirt.h"
+#include "vector.h"
 
 static inline void	put_pixel(char *dst, t_rgb rgb)
 {
-	*(unsigned int *)dst = rgb_to_int(rgb);
+	*(unsigned int *)dst = normalized_rgb_to_int(rgb);
 }
 
-static inline t_rgb	compute_color(size_t x, size_t y)
+static t_rgb	ray_color(const t_ray *r)
 {
-	t_rgb	rgb;
-
-	rgb.r = (unsigned char)(255.999 * ((double)x / (WIN_W - 1)));
-	rgb.g = (unsigned char)(255.999 * ((double)y / (WIN_H - 1)));
-	rgb.b = 0;
-	return (rgb);
+	t_vec3 unit_direction = vec3_normalize(r->dir);
+	double a = 0.5 * (unit_direction.arr[Y] + 1.0);
+	t_rgb color = vec3_scal_mult((t_rgb){{1, 1, 1}}, 1 - a);
+	color = vec3_add(color, vec3_scal_mult((t_rgb){{0.5, 0.7, 1.0}}, a));
+	return (color);
 }
 
 static void	render_frame(const t_renderer *self)
 {
+	// Ray tracing in one weekend
+    double focal_length = 1.0;
+    double viewport_height = 2.0;
+    double viewport_width = viewport_height * (double)WIN_W / WIN_H;
+    t_vec3 camera_center = {{0, 0, 0}};
+	t_vec3 viewport_u = {{viewport_width, 0, 0}};
+	t_vec3 viewport_v = {{0, -viewport_height, 0}};
+	t_vec3 pixel_delta_u = vec3_scal_div(viewport_u, WIN_W);
+	t_vec3 pixel_delta_v = vec3_scal_div(viewport_v, WIN_H);
+	t_vec3 viewport_upper_left = vec3_sub(camera_center, (t_vec3){{0, 0, focal_length}});
+	viewport_upper_left = vec3_sub(viewport_upper_left, vec3_scal_div(viewport_u, 2));
+	viewport_upper_left = vec3_sub(viewport_upper_left, vec3_scal_div(viewport_v, 2));
+	t_vec3 pixel00_loc = vec3_add(viewport_upper_left, vec3_scal_mult(vec3_add(pixel_delta_u, pixel_delta_v), 0.5));
+
 	const int	bpp = self->bits_per_pixel / 8;
 	char		*row;
 	char		*px;
@@ -48,7 +62,10 @@ static void	render_frame(const t_renderer *self)
 		x = 0;
 		while (x < WIN_W)
 		{
-			put_pixel(px, compute_color(x, y));
+			t_vec3 pixel_center = vec3_add(pixel00_loc, vec3_add(vec3_scal_mult(pixel_delta_u, x), vec3_scal_mult(pixel_delta_v, y)));
+			t_vec3 ray_direction = vec3_sub(pixel_center, camera_center);
+			t_ray r = {pixel_center, ray_direction};
+			put_pixel(px, ray_color(&r));
 			px += bpp;
 			x++;
 		}
